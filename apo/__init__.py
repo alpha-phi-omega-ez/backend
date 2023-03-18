edae6ec964bc76031ce9ba00730b423777a453ba
Rafael Cenzano <savagecoder77@gmail.com>
2023-03-18 01:23:39 -0400

    start initialization file

---
+++ apo/__init__.py
@@ -0,0 +1,30 @@
+# Import Flask for flask app object
+from flask import Flask
+
+# Import Flask modules
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+# Create flask app object
+app = Flask(__name__)
+
+
+# Create Database object from brcrypt object
+db = SQLAlchemy(app, session_options={'expire_on_commit': False})
+
+
+# Create Login manager
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'warning'
+
+
+# importing all the models and initializing them
+from apo.models import *
+db.create_all()
+
+
+# Import all views
+import apo.views
