43d32e4ef89d24432c0f92e5a83fc451914d0afe
Rafael Cenzano <savagecoder77@gmail.com>
2023-05-13 23:36:27 -0700

    increase config for login manager

--- apo/__init__.py
+++ apo/__init__.py
@@ -7,6 +7,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 from config import TestingConfig
 
+from oauthlib.oauth2 import WebApplicationClient
+
 
 # Create flask app object
 app = Flask(__name__)
@@ -16,10 +18,14 @@ app.config.from_object(TestingConfig)
 db = SQLAlchemy(app, session_options={'expire_on_commit': False})
 
 # Create Login manager
-login_manager = LoginManager(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'warning'
+login_manager.session_protection = "strong"
 
+# Create OAuth Client
+oauth_client = WebApplicationClient(app.config["GOOGLE_CLIENT_ID"])
 
 # importing all the models and initializing them
 from apo.models import *
