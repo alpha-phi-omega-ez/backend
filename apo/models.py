5baf9b4f6a355b67f01496ab83ad34b16708bf0b
Rafael Cenzano <savagecoder77@gmail.com>
2023-06-21 00:18:48 -0700

    update models with date/times to better track and alias for backtests

--- apo/models.py
+++ apo/models.py
@@ -14,17 +14,21 @@ class BacktestClasses(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
     subject_code = db.Column(db.String(4), nullable=False, unique=False)
     course_number = db.Column(db.Integer, nullable=False, unique=False)
-    name_of_class = db.Column(db.String(100), nullable=False, unique=False)
+    name_of_class = db.Column(db.String(150), nullable=False, unique=False)
+    is_alias = db.Column(db.Boolean, nullable=False, unique=False)
+    alias_subject_code = db.Column(db.String(4), nullable=True, unique=False)
+    alias_course_number = db.Column(db.Integer, nullable=True, unique=False)
 
 
 # Backtest Table
 # Spring semester denoted a for proper sorting
 class Backtest(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    subject_code = db.Column(db.String(4), nullable=False, unique=False)
     added = db.Column(db.DateTime, nullable=False, unique=False)
+    last_modified = db.Column(db.DateTime, nullable=False, unique=False)
+    subject_code = db.Column(db.String(4), nullable=False, unique=False)
     course_number = db.Column(db.Integer, nullable=False, unique=False)
-    name_of_class = db.Column(db.String(100), nullable=False, unique=False)
+    name_of_class = db.Column(db.String(150), nullable=False, unique=False)
     exam = db.Column(db.Boolean, unique=False, default=False)
     quiz = db.Column(db.Boolean, unique=False, default=False)
     midterm = db.Column(db.Boolean, unique=False, default=False)
@@ -37,9 +41,14 @@ class Backtest(db.Model):
 # Spring semester denoted a for proper sorting
 class Chargers(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
+    added = db.Column(db.DateTime, nullable=False, unique=False)
+    last_modified = db.Column(db.DateTime, nullable=False, unique=False)
     in_office = db.Column(db.Boolean, nullable=False, unique=False)
     checked_out = db.Column(db.DateTime, nullable=False, unique=False)
     description = db.Column(db.Text, nullable=False, unique=False)
+    phone_area_code = db.Column(db.Integer, nullable=False, unique=False)
+    phone_middle = db.Column(db.Integer, nullable=False, unique=False)
+    phone_end = db.Column(db.Integer, nullable=False, unique=False)
 
 
 # Lost Reports Table
@@ -55,6 +64,7 @@ class LostReport(db.Model):
     item_type = db.Column(db.String(15), nullable=False, unique=False)
     locations = db.Column(db.Text, nullable=False, unique=False)
     date_lost = db.Column(db.Date, nullable=False, unique=False)
+    date_added = db.Column(db.Date, nullable=False, unique=False)
 
 
 class LostItem(db.Model):
