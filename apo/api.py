fff0b92ef607dc19b8bdec1596e6c75780fde03e
Rafael Cenzano <savagecoder77@gmail.com>
2023-06-29 01:59:27 -0700

    add create and delete api calls

--- apo/api.py
+++ apo/api.py
@@ -52,3 +52,15 @@ def checkout_charger_api():
 @app.route("/api/v1/chargers/admin/checkin", methods=["POST"])
 def checkin_charger_api():
     return chargers.checkin(request.get_json())
+
+
+# @login_required
+@app.route("/api/v1/chargers/admin/create", methods=["POST"])
+def create_charger_api():
+    return chargers.create(request.get_json())
+
+
+# @login_required
+@app.route("/api/v1/chargers/admin/delete", methods=["POST"])
+def delete_charger_api():
+    return chargers.delete(request.get_json())
