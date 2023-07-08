5541513a406a932c2bea9e00d15010270c23ca7f
Rafael Cenzano <savagecoder77@gmail.com>
2023-07-08 15:44:53 -0700

    add alias support and further optimization

--- apo/helpers/backtests.py
+++ apo/helpers/backtests.py
@@ -7,32 +7,34 @@ from apo import app, db
 from apo.models import Backtest, BacktestClasses
 
 
+SUBJECT_CODE_QUERY = "SELECT DISTINCT subject_code FROM backtest_classes"
+SUBJECT_CODE = "subject_code"
+COURSE_NUMBER = "course_number"
+NAME = "name"
+
+
 def list_subject_codes():
-    subject_codes = db.session.execute(
-        text("SELECT DISTINCT subject_code FROM backtest_classes")
-    ).all()
+    subject_codes = db.session.execute(text(SUBJECT_CODE_QUERY)).scalars()
 
     if subject_codes is None:
         app.logger.error("Failed to query and find subject codes")
         abort(500)
 
-    codes = []
-    for code in subject_codes:
-        codes.append(code[0])
+    codes = list(map(lambda x: str(x), subject_codes))
 
-    subject_code_dict = {"subject_codes": list(set(codes))}
+    subject_code_dict = dict(subject_codes=codes)
 
     app.logger.debug(f"subject code list data created {subject_code_dict}")
     return subject_code_dict
 
 
 def list_classes(request_data):
-    if "subject_code" not in request_data:
-        return make_response({"response": f"requires subject_code"}, 400)
+    if SUBJECT_CODE not in request_data:
+        return make_response({"response": "requires subject_code"}, 400)
 
     classes = db.session.execute(
         db.select(BacktestClasses).where(
-            BacktestClasses.subject_code == request_data["subject_code"].upper()
+            BacktestClasses.subject_code == request_data[SUBJECT_CODE].upper()
         )
     ).all()
 
@@ -42,13 +44,31 @@ def list_classes(request_data):
     classes_dict = {}
     for bt_class in classes:
         classes_dict[bt_class.BacktestClasses.id] = {
-            "name": bt_class.BacktestClasses.name_of_class,
-            "course_number": bt_class.BacktestClasses.course_number,
+            NAME: bt_class.BacktestClasses.name_of_class,
+            COURSE_NUMBER: bt_class.BacktestClasses.course_number,
         }
 
     app.logger.debug(f"classes list data created {classes_dict}")
 
     return make_response(classes_dict, 200)
+
+
+def query_backtests(subject_code, course_number):
+    bt_select = (
+        db.select(Backtest)
+        .where(Backtest.subject_code == subject_code)
+        .where(Backtest.course_number == course_number)
+        .order_by(Backtest.year.desc())
+        .order_by(Backtest.semester.desc())
+    )
+
+    exams = db.session.execute(bt_select.where(Backtest.exam == True)).all()
+    quizzes = db.session.execute(bt_select.where(Backtest.quiz == True)).all()
+    midterms = db.session.execute(bt_select.where(Backtest.midterm == True)).all()
+
+    app.logger.debug(f"Backtests quiered for {subject_code} {course_number}")
+
+    return exams, quizzes, midterms
 
 
 def process_backtests(bt):
@@ -63,27 +83,28 @@ def process_backtests(bt):
 
 
 def backtests(request_data):
-    if "subject_code" not in request_data or "course_number" not in request_data:
+    if SUBJECT_CODE not in request_data or COURSE_NUMBER not in request_data:
         return make_response(
-            {"response": f"requires subject_code and course_number"}, 400
+            {"response": "requires subject_code and course_number"}, 400
         )
 
-    subject_code = request_data["subject_code"].upper()
-    course_number = request_data["course_number"]
+    subject_code = request_data[SUBJECT_CODE].upper()
+    course_number = request_data[COURSE_NUMBER]
 
-    bt_select = (
-        db.select(Backtest)
-        .where(Backtest.subject_code == subject_code)
-        .where(Backtest.course_number == course_number)
-        .order_by(Backtest.year.desc())
-        .order_by(Backtest.semester.desc())
-    )
+    course = db.session.execute(
+        db.select(BacktestClasses)
+        .where(BacktestClasses.subject_code == subject_code)
+        .where(BacktestClasses.course_number == course_number)
+    ).first()
 
-    exams = db.session.execute(bt_select.where(Backtest.exam == True)).all()
-    quizzes = db.session.execute(bt_select.where(Backtest.quiz == True)).all()
-    midterms = db.session.execute(bt_select.where(Backtest.midterm == True)).all()
+    if course is None:
+        abort(404)
 
-    app.logger.debug(f"Backtests quiered for {subject_code} {course_number}")
+    if course.BacktestClasses.is_alias:
+        subject_code = course.BacktestClasses.alias_subject_code
+        course_number = course.BacktestClasses.alias_course_number
+
+    exams, quizzes, midterms = query_backtests(subject_code, course_number)
 
     if exams is None and quizzes is None and midterms is None:
         abort(404)
