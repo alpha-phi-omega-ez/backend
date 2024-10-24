c749363cb379762942674bf63d67e72e90b513ab
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-10-24 03:20:57 -0400

    remove uneeded fields/models

--- server/models/loanertech.py
+++ server/models/loanertech.py
@@ -1,36 +1,22 @@
-from typing import Any, Optional
+from typing import Optional
 
 from pydantic import BaseModel, EmailStr, Field
 
 
 class LoanerTechCheckout(BaseModel):
-    id: int = Field(...)
     phone_number: str = Field(..., max_length=12)
     email: Optional[EmailStr] = Field(...)
 
     class Config:
         schema_extra = {
             "example": {
-                "id": 0,
                 "phone_number": "518-276-6516",
                 "email": "glump@rpi.edu",
             }
         }
 
 
-class LoanerTechCheckIn(BaseModel):
-    id: int = Field(...)
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "id": 0,
-            }
-        }
-
-
 class LoanerTech(BaseModel):
-    id: int = Field(...)
     description: str = Field(...)
 
     class Config:
