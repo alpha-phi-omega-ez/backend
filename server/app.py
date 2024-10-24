f3bf7f33ba486d39cdb429988d72e1efaead7322
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-10-24 03:20:01 -0400

    update CORS policy

--- server/app.py
+++ server/app.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from server.routes.loanertech import router as LoanerTechRouter
 
 app = FastAPI()
+
+# Define the origins that are allowed to make requests to your FastAPI app
+origins = [
+    "http://localhost:3000",  # Frontend URL
+    # Add other origins as needed
+]
+
+# Add CORS middleware to FastAPI
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow specific origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers (authorization, etc.)
+)
 
 app.include_router(LoanerTechRouter, tags=["LoanerTech"], prefix="/loanertech")
 
