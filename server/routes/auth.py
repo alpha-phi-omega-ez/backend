5fbf05cfe42cfc591259053b3aa791c06cf9d5b7
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-11-20 02:23:42 -0500

    Update auth routes with logout functionality and auth check moved helper functions to helper function file

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,14 +1,21 @@
-from datetime import datetime, timedelta, timezone
-from uuid import uuid4
+from datetime import datetime, timedelta
 
-from fastapi import APIRouter, Body, HTTPException
-from fastapi.responses import RedirectResponse, Response
+import jwt
+from fastapi import APIRouter, Body, HTTPException, Request
+from fastapi.responses import JSONResponse, RedirectResponse, Response
 from fastapi_sso.sso.google import GoogleSSO
-from jwt import encode as jwt_encode
 from starlette import status
-from starlette.requests import Request
+from starlette.requests import Request as StarletteRequest
 
 from server.config import get_settings
+from server.helpers.auth import (
+    BlacklistedTokenException,
+    blacklist_token,
+    create_access_token,
+    generate_temporary_code,
+    validate_code_and_get_user_email,
+    validate_token,
+)
 from server.models.auth import TokenRequest
 
 settings = get_settings()
@@ -21,62 +28,31 @@ google_sso = GoogleSSO(
 
 router = APIRouter()
 
-temp_codes = {}
-
-
-async def generate_temporary_code(user_email: str) -> str:
-    # Generate a unique temporary code
-    code = str(uuid4())
-    expires_at = datetime.now() + timedelta(seconds=5)  # expires in 5 seconds
-    temp_codes[code] = {"email": user_email, "expires_at": expires_at}
-    return code
-
-
-async def validate_code_and_get_user_email(code: str) -> str | None:
-    token_data = temp_codes.get(code, {})
-    if not token_data:
-        return None
-
-    user_email = token_data.get("email", None)
-    expire = token_data.get("expires_at", None)
-
-    if user_email and expire and expire > datetime.now():
-        # If found, delete the code to prevent reuse
-        del temp_codes[code]
-        return user_email
-    elif expire:
-        # If the code has expired, delete it
-        del temp_codes[code]
-
-    return None
-
-
-async def create_access_token(
-    data: dict, expires_delta: timedelta | None = None
-) -> str:
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt_encode(
-        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
-    )
-    return encoded_jwt
-
 
 @router.get("/login", response_description="Initiate login with Google url")
-async def google_login() -> RedirectResponse:
+async def google_login(request: Request) -> RedirectResponse:
+    redirect_url = request.query_params.get("redirect", "/")
+
+    if settings.TESTING:
+        print("TEST LOGIN")
+        code = await generate_temporary_code("test@apoez.org")
+        return RedirectResponse(
+            url=f"{settings.FRONTEND_URL}/login/callback?code={code}&redirect={redirect_url}"
+        )
+
     with google_sso:
-        login_stuff = await google_sso.get_login_redirect()
+        login_stuff = await google_sso.get_login_redirect(
+            params={"redirect": redirect_url}
+        )
     return login_stuff
 
 
 @router.get("/callback", response_description="Google callback")
-async def google_callback(request: Request, response: Response):
+async def google_callback(star_request: StarletteRequest, request: Request):
+    redirect_url = request.query_params.get("redirect", "/")
+
     with google_sso:
-        user = await google_sso.verify_and_process(request)
+        user = await google_sso.verify_and_process(star_request)
 
     if user is None or user.email is None:
         return RedirectResponse(
@@ -85,7 +61,9 @@ async def google_callback(request: Request, response: Response):
         )
 
     code = await generate_temporary_code(user.email)
-    return RedirectResponse(url=f"{settings.FRONTEND_URL}/login/callback?code={code}")
+    return RedirectResponse(
+        url=f"{settings.FRONTEND_URL}/login/callback?code={code}&redirect={redirect_url}"
+    )
 
 
 @router.post("/token")
@@ -97,10 +75,53 @@ async def exchange_code_for_token(token: TokenRequest = Body(...)):
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = await create_access_token(
         data={
-            "sub": user_email,
+            "email": user_email,
             "datetime": datetime.now().strftime("%A %B %d %Y %H %M %S %f %j"),
         },
         expires_delta=access_token_expires,
     )
-    print(access_token)
-    return {"access_token": access_token, "token_type": "bearer"}
+
+    secure = not settings.TESTING
+    response = JSONResponse(content={"message": "Login successful"})
+    response.set_cookie(
+        key="authToken",
+        value=access_token,
+        httponly=True,  # Prevent JavaScript access
+        secure=secure,  # Use HTTPS in production
+        samesite="strict",  # Prevent cross-site requests
+        max_age=settings.ACCESS_TOKEN_EXPIRE_MINUTES,
+    )
+    return response
+
+
+@router.post("/logout")
+async def logout(request: Request, response: Response) -> JSONResponse:
+    # Add the token to a blacklist or invalidation list
+    token = request.cookies.get("authToken")
+    if token:
+        # Assuming you have a blacklist set or database table
+        # Here we use a simple set for demonstration purposes
+        await blacklist_token(token)
+        response.delete_cookie("authToken")
+        return JSONResponse(content={"message": "Logged out", "success": True})
+    return JSONResponse(
+        content={"message": "No token found to log out", "success": False}
+    )
+
+
+@router.get("/auth/check")
+async def check_auth(request: Request):
+    token = request.cookies.get("authToken")
+
+    if token:
+        try:
+            await validate_token(token)  # Your JWT validation function
+            return {"authenticated": True}
+        except (
+            jwt.ExpiredSignatureError,
+            jwt.InvalidTokenError,
+            BlacklistedTokenException,
+        ):
+            return {"authenticated": False}
+
+    return {"authenticated": False}
