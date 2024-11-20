7d7ce1393606ecc69df58d57459202c6ed30cea9
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-11-20 02:18:15 -0500

    Update models for checkout and make checkin request model

--- server/models/loanertech.py
+++ server/models/loanertech.py
@@ -2,16 +2,29 @@ from typing import Optional
 
 from pydantic import BaseModel, EmailStr, Field
 
-
 class LoanerTechCheckout(BaseModel):
+    ids: list[int] = Field(...)
     phone_number: str = Field(..., max_length=12)
     email: Optional[EmailStr] = Field(...)
+    name: str = Field(...)
 
     class Config:
         json_schema_extra = {
             "example": {
+                "ids": [1, 4],
                 "phone_number": "518-276-6516",
                 "email": "glump@rpi.edu",
+                "name": "Alfred Glump",
+            }
+        }
+
+class LoanerTechCheckin(BaseModel):
+    ids: list[int] = Field(...)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "ids": [1, 4],
             }
         }
 
