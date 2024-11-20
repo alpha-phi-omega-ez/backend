52aec9382a9c505ccb18ab16643cd357c5c2e2c3
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-11-20 02:23:12 -0500

    update routes to handle frontend calls

--- server/routes/loanertech.py
+++ server/routes/loanertech.py
@@ -1,31 +1,57 @@
 from typing import Any
 
-from fastapi import APIRouter, Body
+import jwt
+from fastapi import APIRouter, Body, HTTPException, Request
 from fastapi.encoders import jsonable_encoder
 
+from server.config import get_settings
 from server.database.loanertech import (
     add_loanertech,
     delete_loanertech,
     retrieve_loanertech,
     retrieve_loanertechs,
+    retrieve_loanertechs_unauthenticated,
     update_loanertech,
 )
+from server.helpers.auth import (
+    BlacklistedTokenException,
+    validate_token,
+    simple_auth_check,
+)
 from server.models import ErrorResponseModel, ResponseModel
-from server.models.loanertech import LoanerTech, LoanerTechCheckout
+from server.models.loanertech import LoanerTech, LoanerTechCheckin, LoanerTechCheckout
 
 router = APIRouter()
 
+settings = get_settings()
+
 
 @router.get("/", response_description="LoanerTech list retrieved")
-async def get_loanertechs():
-    loanertechs = await retrieve_loanertechs()
+async def get_loanertechs(request: Request):
+    authenticated, _, _ = await simple_auth_check(request)
+
+    loanertechs = []
+    if authenticated:
+        loanertechs = await retrieve_loanertechs()
+    else:
+        loanertechs = await retrieve_loanertechs_unauthenticated()
     if loanertechs:
-        return ResponseModel(loanertechs, "LoanerTech data retrieved successfully")
-    return ResponseModel(loanertechs, "Empty list returned")
+        return ResponseModel(
+            loanertechs, "LoanerTech data retrieved successfully", authenticated
+        )
+    return ResponseModel(loanertechs, "Empty list returned", authenticated)
 
 
 @router.post("/", response_description="LoanerTech data added into the database")
-async def add_loanertech_data(loanertech: LoanerTech = Body(...)) -> dict[str, Any]:
+async def add_loanertech_data(
+    request: Request, loanertech: LoanerTech = Body(...)
+) -> dict[str, Any]:
+    authenticated, message, payload = await simple_auth_check(request)
+    if not authenticated:
+        raise HTTPException(status_code=401, detail=message)
+
+    print(payload)
+
     dict_loanertech = jsonable_encoder(loanertech)
     new_loanertech = await add_loanertech(dict_loanertech)
     return ResponseModel(new_loanertech, "LoanerTech added successfully.")
@@ -39,8 +65,16 @@ async def get_loanertech_data(id: int):
     return ErrorResponseModel("An error occurred.", 404, "LoanerTech doesn't exist.")
 
 
-@router.put("/{id}")
-async def update_loanertech_data(id: int, req: LoanerTech = Body(...)):
+@router.put("/update/{id}")
+async def update_loanertech_data(
+    request: Request, id: int, req: LoanerTech = Body(...)
+):
+    authenticated, message, payload = await simple_auth_check(request)
+    if not authenticated:
+        raise HTTPException(status_code=401, detail=message)
+
+    print(payload)
+
     dict_req = {k: v for k, v in req.model_dump().items() if v is not None}
     updated_loanertech = await update_loanertech(id, dict_req)
     if updated_loanertech:
@@ -56,9 +90,15 @@ async def update_loanertech_data(id: int, req: LoanerTech = Body(...)):
 
 
 @router.delete(
-    "/{id}", response_description="LoanerTech data deleted from the database"
+    "/delete/{id}", response_description="LoanerTech data deleted from the database"
 )
-async def delete_loanertech_data(id: int):
+async def delete_loanertech_data(request: Request, id: int):
+    authenticated, message, payload = await simple_auth_check(request)
+    if not authenticated:
+        raise HTTPException(status_code=401, detail=message)
+
+    print(payload)
+
     deleted_loanertech = await delete_loanertech(id)
     if deleted_loanertech:
         return ResponseModel(
@@ -69,34 +109,75 @@ async def delete_loanertech_data(id: int):
     )
 
 
-@router.put("/checkout/{id}")
-async def checkout_loanertech(id: int, req: LoanerTechCheckout = Body(...)):
+@router.put("/checkout")
+async def checkout_loanertech(request: Request, req: LoanerTechCheckout = Body(...)):
+    authenticated, message, _ = await simple_auth_check(request)
+    print(authenticated)
+    print(message)
+    if not authenticated:
+        raise HTTPException(status_code=401, detail=message)
+
     dict_req = {k: v for k, v in req.model_dump().items() if v is not None}
-    dict_req["in_office"] = False
-    updated_loanertech = await update_loanertech(id, dict_req)
-    if updated_loanertech:
+
+    print(dict_req)
+
+    success = True
+    ids = []
+    for id in dict_req["ids"]:
+        item = {
+            "in_office": False,
+            "phone": dict_req["phone_number"],
+            "email": dict_req["email"],
+            "name": dict_req["name"],
+        }
+        updated_loanertech = await update_loanertech(id, item)
+        ids.append(id)
+        if not updated_loanertech:
+            success = False
+            break
+
+    if success:
         return ResponseModel(
-            f"LoanerTech with ID: {id} checked out",
+            f"LoanerTech with IDs: {ids} checked out",
             "LoanerTech checked out successfully",
         )
     return ErrorResponseModel(
         "An error occurred",
-        404,
+        500,
         "There was an error checking out the loanertech data.",
     )
 
 
-@router.put("/checkin/{id}")
-async def checkin_loanertech(id: int):
-    dict_req = {"phone": "", "email": "", "in_office": True}
-    updated_loanertech = await update_loanertech(id, dict_req)
-    if updated_loanertech:
+@router.put("/checkin")
+async def checkin_loanertech(request: Request, req: LoanerTechCheckin = Body(...)):
+    authenticated, message, _ = await simple_auth_check(request)
+    if not authenticated:
+        raise HTTPException(status_code=401, detail=message)
+
+    dict_req = {k: v for k, v in req.model_dump().items() if v is not None}
+
+    success = True
+    ids = []
+    for id in dict_req["ids"]:
+        item = {
+            "in_office": True,
+            "phone": "",
+            "email": "",
+            "name": "",
+        }
+        updated_loanertech = await update_loanertech(id, item)
+        ids.append(id)
+        if not updated_loanertech:
+            success = False
+            break
+
+    if success:
         return ResponseModel(
-            f"LoanerTech with ID: {id} checked in",
+            f"LoanerTech with IDs: {ids} checked in",
             "LoanerTech checked in successfully",
         )
     return ErrorResponseModel(
         "An error occurred",
-        404,
+        500,
         "There was an error checking in the loanertech data.",
     )
