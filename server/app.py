e57b9e90704d3a36730856e8842c603352df6d58
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-12-27 00:02:45 -0800

    add lifespan for setup and shutdown logic

--- server/app.py
+++ server/app.py
@@ -1,12 +1,26 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from server.config import settings
+from server.database import db_setup
 from server.routes.auth import router as AuthRouter
-from server.routes.loanertech import router as LoanerTechRouter
 from server.routes.backtest import router as BacktestRouter
+from server.routes.laf import router as LAFRouter
+from server.routes.loanertech import router as LoanerTechRouter
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # This runs during the startup phase
+    await db_setup()
+    yield  # Application runs here
+    # This runs during the shutdown phase
+    # Any cleanup logic can go here
+
+
+app = FastAPI(lifespan=lifespan)
 
 # Add CORS middleware to FastAPI
 app.add_middleware(
@@ -17,8 +31,10 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers (authorization, etc.)
 )
 
+
 app.include_router(LoanerTechRouter, tags=["LoanerTech"], prefix="/loanertech")
 app.include_router(BacktestRouter, tags=["Backtests"], prefix="")
+app.include_router(LAFRouter, tags=["laf"], prefix="/laf")
 app.include_router(AuthRouter, tags=["Auth"], prefix="")
 
 
