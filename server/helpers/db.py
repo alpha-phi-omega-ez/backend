3eab8821d0ec1150f745ab4a35325fdcd2e4ac07
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-12-29 22:24:10 -0800

    new timedelta helper function

--- server/helpers/db.py
+++ server/helpers/db.py
@@ -1,3 +1,10 @@
+from datetime import datetime, timedelta
+
+
 async def async_dict_itr(dict_data: dict):
     for k, v in dict_data.items():
         yield k, v
+
+
+async def datetime_time_delta(now: datetime, days: int) -> str:
+    return (now - timedelta(days=days)).strftime("%Y-%m-%d")
