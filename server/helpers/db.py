1a3f33b8ada7e86a75c29c34521e7bed9245367f
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2025-01-05 10:01:34 -0800

    Update to use one function for sequential ids

--- server/helpers/db.py
+++ server/helpers/db.py
@@ -8,3 +8,13 @@ async def async_dict_itr(dict_data: dict):
 
 async def datetime_time_delta(now: datetime, days: int) -> str:
     return (now - timedelta(days=days)).strftime("%Y-%m-%d")
+
+
+async def get_next_sequence_value(sequence_name, sequence_id_collection) -> int:
+    result = await sequence_id_collection.find_one_and_update(
+        {"_id": sequence_name}, {"$inc": {"seq": 1}}, return_document=True
+    )
+    if result is None:
+        await sequence_id_collection.insert_one({"_id": sequence_name, "seq": 1})
+        result = {"seq": 1}
+    return result["seq"]
