c2ec801f1738614c147a548f04fcd82cda813815
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2025-01-05 10:02:57 -0800

    Add sorting, typing, and error checking

--- server/database/loanertech.py
+++ server/database/loanertech.py
@@ -1,10 +1,13 @@
+from fastapi import HTTPException, status
+
 from server.database import database
+from server.helpers.db import get_next_sequence_value
 
+sequence_id_collection = database.get_collection("sequence_id")
 loanertech_collection = database.get_collection("loanertech_collection")
-loanertech_id_collection = database.get_collection("loanertech_id_collection")
 
 
-def loanertech_helper(loanertech) -> dict:
+def loanertech_helper(loanertech: dict) -> dict:
     return {
         "id": loanertech["_id"],
         "in_office": loanertech["in_office"],
@@ -15,7 +18,7 @@ def loanertech_helper(loanertech) -> dict:
     }
 
 
-def loanertech_helper_unprotected(loanertech) -> dict:
+def loanertech_helper_unprotected(loanertech: dict) -> dict:
     return {
         "id": loanertech["_id"],
         "in_office": loanertech["in_office"],
@@ -23,28 +26,18 @@ def loanertech_helper_unprotected(loanertech) -> dict:
     }
 
 
-async def get_next_sequence_value(sequence_name) -> int:
-    result = await loanertech_id_collection.find_one_and_update(
-        {"_id": sequence_name}, {"$inc": {"seq": 1}}, return_document=True
-    )
-    if result is None:
-        await loanertech_id_collection.insert_one({"_id": sequence_name, "seq": 1})
-        result = {"seq": 1}
-    return result["seq"]
-
-
 # Retrieve all loaner tech items present in the database with data for unauthenticated users
-async def retrieve_loanertechs_unauthenticated():
+async def retrieve_loanertechs_unauthenticated() -> list[dict]:
     loanertechs = []
-    async for loanertech in loanertech_collection.find():
+    async for loanertech in loanertech_collection.find().sort("_id"):
         loanertechs.append(loanertech_helper_unprotected(loanertech))
     return loanertechs
 
 
 # Retrieve all loaner tech items present in the database with data for authenticated users
-async def retrieve_loanertechs():
+async def retrieve_loanertechs() -> list[dict]:
     loanertechs = []
-    async for loanertech in loanertech_collection.find():
+    async for loanertech in loanertech_collection.find().sort("_id"):
         loanertechs.append(loanertech_helper(loanertech))
     return loanertechs
 
@@ -52,7 +45,9 @@ async def retrieve_loanertechs():
 # Add a new loanertech item into to the database
 async def add_loanertech(loanertech_data: dict) -> dict:
     # Add the ID to the loanertech data
-    loanertech_data["_id"] = await get_next_sequence_value("loanertechid")
+    loanertech_data["_id"] = await get_next_sequence_value(
+        "loanertech_id", sequence_id_collection
+    )
     loanertech_data["in_office"] = True
     loanertech_data["phone"] = ""
     loanertech_data["email"] = ""
@@ -62,11 +57,16 @@ async def add_loanertech(loanertech_data: dict) -> dict:
     new_loanertech = await loanertech_collection.find_one(
         {"_id": loanertech.inserted_id}
     )
-    return loanertech_helper(new_loanertech)
+    if new_loanertech:
+        return loanertech_helper(new_loanertech)
+    raise HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail="Failed to create Loanertech",
+    )
 
 
 # Retrieve a loanertech item with a matching ID
-async def retrieve_loanertech(id: int):
+async def retrieve_loanertech(id: int) -> dict | None:
     loanertech = await loanertech_collection.find_one({"_id": id})
     if loanertech:
         return loanertech_helper(loanertech)
@@ -74,7 +74,7 @@ async def retrieve_loanertech(id: int):
 
 
 # Update a loanertech item with a matching ID
-async def update_loanertech(id: int, data: dict):
+async def update_loanertech(id: int, data: dict) -> bool:
     # Return false if an empty request body is sent.
     if len(data) < 1:
         return False
@@ -89,7 +89,7 @@ async def update_loanertech(id: int, data: dict):
 
 
 # Delete a student from the database
-async def delete_loanertech(id: int):
+async def delete_loanertech(id: int) -> bool:
     student = await loanertech_collection.find_one({"_id": id})
     if student:
         await loanertech_collection.delete_one({"_id": id})
