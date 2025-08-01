5e9db41edf1b3541a068f134274506e0c47932ba
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2025-08-01 00:42:33 -0700

    remove functions not needed due to valkey upgrade and adjust other functions to use valkey

--- server/helpers/auth.py
+++ server/helpers/auth.py
@@ -1,48 +1,20 @@
 from datetime import datetime, timedelta, timezone
-from uuid import uuid4
 
 import jwt
 from fastapi import HTTPException, Request, status
 
 from server.config import settings
+from server.database.valkey import is_token_blacklisted
 
 temp_codes = {}
-blacklisted_tokens = set()
 
 
 class BlacklistedTokenException(Exception):
     pass
 
 
-async def generate_temporary_code(user_email: str) -> str:
-    # Generate a unique temporary code
-    code = str(uuid4())
-    expires_at = datetime.now() + timedelta(seconds=8)  # expires in 8 seconds
-    temp_codes[code] = {"email": user_email, "expires_at": expires_at}
-    return code
-
-
-async def validate_code_and_get_user_email(code: str) -> str | None:
-    token_data = temp_codes.get(code, {})
-    if not token_data:
-        return None
-
-    user_email = token_data.get("email", None)
-    expire = token_data.get("expires_at", None)
-
-    if user_email and expire and expire > datetime.now():
-        # If found, delete the code to prevent reuse
-        del temp_codes[code]
-        return user_email
-    elif expire:
-        # If the code has expired, delete it
-        del temp_codes[code]
-
-    return None
-
-
-async def validate_token(token: str) -> dict:
-    if token in blacklisted_tokens:
+async def validate_token(request: Request, token: str) -> dict:
+    if await is_token_blacklisted(request, token):
         raise BlacklistedTokenException
 
     payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
@@ -65,16 +37,12 @@ async def create_access_token(
     return encoded_jwt
 
 
-async def blacklist_token(token: str) -> None:
-    blacklisted_tokens.add(token)
-
-
 async def simple_auth_check(request: Request) -> tuple[bool, str, dict | None]:
     token = request.cookies.get("authToken")
 
     if token:
         try:
-            payload = await validate_token(token)
+            payload = await validate_token(request, token)
             return True, "", payload
         except jwt.ExpiredSignatureError:
             return False, "Token expired", None
