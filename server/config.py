2614cda4b7182111dd65b0b74d0158913f437f10
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2026-03-05 23:15:53 -0500

    feat: update secret key random default

--- server/config.py
+++ server/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID", "")
     GOOGLE_CLIENT_SECRET: str = os.getenv("GOOGLE_CLIENT_SECRET", "")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 4
-    SECRET_KEY: str = str(os.urandom(32))
+    SECRET_KEY: str = os.urandom(32).hex()
     ALGORITHM: str = "HS256"
     TESTING: bool = False
 
